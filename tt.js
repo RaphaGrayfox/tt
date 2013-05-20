55214de67e8bb3e053040057a293109ba838f1e2
William Bruno <rocha_bruno@hotmail.com>
2013-05-20 20:23:16 -0300

    initial

---
+++ tt.js
@@ -0,0 +1,32 @@
+;(function(){
+	"use strict";
+	var tt = {};
+	tt.init = function(objs) {
+		var i = objs.length;
+		while(i--) {
+			var obj = objs[i],
+				wrap = tt.createWrap(obj),
+				ttContent = tt.getContent(obj);
+
+			wrap.appendChild(ttContent);
+		}
+	};
+	tt.createWrap = function(obj) {
+		var wrap = document.createElement('span');
+		wrap.className = 'tt-wrap';
+		obj.parentNode.replaceChild(wrap, obj);
+		wrap.appendChild(obj);
+		return wrap;
+	};
+	tt.getContent = function(obj) {
+		var rel = obj.getAttribute('data-rel');
+		return document.querySelector(rel);
+	};
+	window.addEventListener("load", function() {
+		tt.init($('.tt'));
+		tt.init($('#another'));
+	});
+	var $ = function(selector) {
+		return document.querySelectorAll(selector);
+	};
+}());
